507c186f1cac65690f0ae7a8df49f15caaeef988
savindu19990928 <devriousteam@gmail.com>
2023-12-15 03:13:44 +0530

    Version 1

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(5000, ()=> {
+    console.log("Server is running on http://localhost:5000")
+});
